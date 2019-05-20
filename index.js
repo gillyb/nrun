388ec8600d44cd5f0ced0d68b8e75ddba8857d01
Gilly Barr <gillyb@google.com>
2019-05-20 17:09:42 +0300

    Run script by defining second argument
    Now you can run a script by adding the script name to run as a command line argument like this: `nrun dev` will run the 'dev' script.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const packageObject = JSON.parse(packageFile.toString());
 
 if (!packageObject.scripts) {
   terminal.yellow('\n\n  This package does not have any defined scripts\n\n');
-  process.exit(0);
+  process.exit(1);
 }
 
 const scripts = Object.keys(packageObject.scripts);
@@ -23,39 +23,54 @@ const runScript = scriptToRun => {
   spawn('npm run ' + scripts[scriptToRun], { shell: true, stdio: 'inherit' });
 };
 
-terminal.hideCursor().singleColumnMenu(menuOptions, {
-  selectedLeftPadding: ' > ',
-  leftPadding: '   ',
-  cancelable: true,
-  selectedStyle: terminal.bold,
-  submittedStyle: terminal.bold,
-  keyBindings: {
-    ENTER: 'submit',
-    KP_ENTER: 'submit',
-    UP: 'previous',
-    K: 'previous',
-    k: 'previous',
-    DOWN: 'next',
-    J: 'next',
-    j: 'next',
-    TAB: 'cycleNext',
-    SHIFT_TAB: 'cyclePrevious',
-    HOME: 'first',
-    END: 'last',
-    BACKSPACE: 'cancel',
-    DELETE: 'cancel',
-    ESCAPE: 'escape',
-    CTRL_C: 'escape'
-  }
-}, (err, response) => {
-
-  terminal.hideCursor(false).grabInput(false);
-
-  if (response.selectedIndex === null || response.selectedIndex === undefined) {
+// Check if we got an argument for a specific script to run
+const args = process.argv;
+if (args.length === 3) {
+  if (!~scripts.indexOf(args[2])) {
+    terminal.yellow('\n  The script \'' + args[2] + '\' does not exist in package.json file\n\n');
     process.exit(1);
-    return;
   }
 
-  runScript(response.selectedIndex);
+  console.log('Running script: ' + args[2]);
+  runScript(scripts.indexOf(args[2]));
+}
+else {
+  // We didn't get anything, just show the menu of options
+  terminal.hideCursor().singleColumnMenu(menuOptions, {
+    selectedLeftPadding: ' > ',
+    leftPadding: '   ',
+    cancelable: true,
+    selectedStyle: terminal.bold,
+    submittedStyle: terminal.bold,
+    keyBindings: {
+      ENTER: 'submit',
+      KP_ENTER: 'submit',
+      UP: 'previous',
+      K: 'previous',
+      k: 'previous',
+      DOWN: 'next',
+      J: 'next',
+      j: 'next',
+      TAB: 'cycleNext',
+      SHIFT_TAB: 'cyclePrevious',
+      HOME: 'first',
+      END: 'last',
+      BACKSPACE: 'cancel',
+      DELETE: 'cancel',
+      ESCAPE: 'escape',
+      CTRL_C: 'escape'
+    }
+  }, (err, response) => {
 
-});
+    terminal.hideCursor(false).grabInput(false);
+
+    if (response.selectedIndex === null || response.selectedIndex === undefined) {
+      terminal.red('\n  Unknown Error!\n\n');
+      process.exit(1);
+      return;
+    }
+
+    runScript(response.selectedIndex);
+
+  });
+}
